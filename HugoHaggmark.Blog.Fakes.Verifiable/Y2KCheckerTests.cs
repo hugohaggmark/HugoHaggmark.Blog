b6e0e89a3e908303e1b0cd41e0618cedcc876467
Hugo Haggmark <hugo.haggmark@gmail.com>
2012-03-11 07:47:50 +0100

    Initial Check-In for blog post

---
+++ HugoHaggmark.Blog.Fakes.Verifiable/Y2KCheckerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HugoHaggmark.Blog.Fakes.Verifiable
+{
+    public static class Y2KChecker
+    {
+        public static void Check()
+        {
+            if (DateTime.Now == new DateTime(2000, 1, 1))
+                throw new ApplicationException("y2kbug!");
+        }
+    }
+
+    [TestClass]
+    public class Y2KCheckerTests_Step1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void Check_WhenCalledWithTheDate_2000_1_1_ThenApplicationExceptionIsThrowned()
+        {
+            using(ShimsContext.Create())
+            {
+                ShimDateTime.NowGet = () => { return new DateTime(2000, 1, 1); };
+
+                Y2KChecker.Check();
+            }
+        }
+    }
+}
