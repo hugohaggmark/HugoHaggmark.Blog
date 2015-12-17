c7ece1b1efcf42ec67ae379463c795361c59235f
Hugo <hugo.haggmark@gmail.com>
2015-12-17 06:48:55 +0100

    added nancy enum as string project, not working because of missing reference

---
+++ HugoHaggmark.Nancy.Enums.As.Strings/Modules/Index.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using System.Collections.Generic;
+
+namespace HugoHaggmark.Nancy.Enums.As.Strings.Modules
+{
+    public class Index : NancyModule
+    {
+        public Index()
+        {
+            Get["/"] = _ =>
+            {
+                var teamMembers = new List<TeamMember>()
+                {
+                    new TeamMember { Name = "Marcus", Avatar = StarWarsCharacter.Luke  },
+                    new TeamMember { Name = "Hugo", Avatar = StarWarsCharacter.Chewbacca }
+                };
+
+                return Response.AsJson(teamMembers);
+            };
+        }
+    }
+
+    public enum StarWarsCharacter
+    {
+        Leia,
+        Luke,
+        Chewbacca
+    }
+
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public StarWarsCharacter Avatar { get; set; }
+    }
+}
